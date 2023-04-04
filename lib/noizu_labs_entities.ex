77a6bbcd7e640aed0db506149c30a991a77b8f80
Keith Brings <keith.brings@noizu.com>
2023-04-04 08:50:42 +0700

    Initial Commit

---
+++ lib/noizu_labs_entities.ex
@@ -0,0 +1,18 @@
+defmodule NoizuLabsEntities do
+  @moduledoc """
+  Documentation for `NoizuLabsEntities`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NoizuLabsEntities.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/noizu_labs_entities_test.exs
@@ -0,0 +1,8 @@
+defmodule NoizuLabsEntitiesTest do
+  use ExUnit.Case
+  doctest NoizuLabsEntities
+
+  test "greets the world" do
+    assert NoizuLabsEntities.hello() == :world
+  end
+end
